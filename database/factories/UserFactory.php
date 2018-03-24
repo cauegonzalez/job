6a57b64fa81ba8db41b77f6f0776efe85473af22
Caue Gonzalez <cauegonzalez@gmail.com>
2018-03-24 00:56:39 -0300

    Criando as seeds de usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Faker\Provider\Address as Address;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,13 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'city' => $faker->city,
+        'state' => $faker->state,
+        'postal_code' => $faker->numberBetween(10000000, 99999999),
+        'address' => $faker->sentence,
+        'number' => $faker->numberBetween(1, 9999),
+        'complement' => $faker->word,
+        'district' => $faker->name,
         'remember_token' => str_random(10),
     ];
 });
